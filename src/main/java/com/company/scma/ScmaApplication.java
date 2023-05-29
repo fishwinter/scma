4739fc5c61cab510a448e0758b0a1d35be22b78b
fishwinter <yxy602677@qq.com>
2023-05-29 10:55:41 +0800

    初始化

---
+++ src/main/java/com/company/scma/ScmaApplication.java
@@ -0,0 +1,13 @@
+package com.company.scma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScmaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScmaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/company/scma/ScmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.scma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScmaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
