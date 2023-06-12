420f24ff2aebdf15af7393cef197979024bb0aa2
fishwinter <yxy602677@qq.com>
2023-06-12 20:45:00 +0800

    合作企业模块开发

---
+++ src/main/java/com/company/scma/common/po/TSysConfig.java
@@ -0,0 +1,57 @@
+package com.company.scma.common.po;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+
+/**
+ * t_sysconfig
+ * @author 
+ */
+@Data
+public class TSysConfig implements Serializable {
+    /**
+     * 主键
+     */
+    @TableId
+    private Integer serialno;
+
+    /**
+     * 设置代码
+     */
+    private String custCode;
+
+    /**
+     * 设置值
+     */
+    private String custValue;
+
+    /**
+     * 创建时间
+     */
+    private Date buildDate;
+
+    /**
+     * 修改时间
+     */
+    private Date modifyDate;
+
+    /**
+     * 修改者userid
+     */
+    private Integer modifyUserid;
+
+    /**
+     * 简介
+     */
+    private String introduction;
+
+    /**
+     * 删除标记：0-删除，1-正常
+     */
+    private Integer deleteflag;
+
+    private static final long serialVersionUID = 1L;
+}
